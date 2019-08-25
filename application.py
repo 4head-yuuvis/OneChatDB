e7bde612bb3f88d618ab7d8f21863a697e4080e3
Andrew Kim <hjk545@gmail.com>
2019-08-25 03:21:17 -0500

    Google Translate API

--- application.py
+++ application.py
@@ -1,13 +1,25 @@
 # template taken from https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-flask.html
 
 from flask import Flask, jsonify, request, render_template
+from google.cloud import translate
+from flask_cors import CORS
 import requests
 import json
 import os
 import time
 import sys
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/andrew/Downloads/onechat-1566682458777-50c2ac0e1c1b.json"
+
+
+# project_id = 'onechat-1566682458777'
+# text = 'Text you wish to translate'
+# location = 'global'
+
+
+
 application = Flask(__name__)
+CORS(application)
 
 headerDict = {}
 paramDict = {}
@@ -24,6 +36,43 @@ def hello_world():
 @application.route('/yuuvis-api')
 def yuuvis_api():
     return (response.text)
+
+# def add_cors_headers(response):
+#     response.headers["Access-Control-Allow-Origin"] = "*"
+#     response.headers["Access-Control-Allow-Credentials"] = "true"
+#     response.headers["Access-Control-Allow-Headers"] = "application/x-www-form-urlencoded"
+#     response.headers["Access-Control-Allow-Methods"] = "GET"
+#     return response
+
+
+# application.after_request(add_cors_headers)
+
+@application.route('/translate')
+def run_quickstart():
+    # [START translate_quickstart]
+    # Imports the Google Cloud client library
+    message = request.args.get('message')
+    tgt = request.args.get('tgt')
+
+    # Instantiates a client
+    translate_client = translate.Client()
+
+    # The text to translate
+    text = u'{}'.format(message)
+    # The target language
+    target = tgt
+
+    # Translates some text into Russian
+    translation = translate_client.translate(
+        text,
+        target_language=target)
+
+    # with open('../chatApp/src/components/output.txt', 'w') as file:
+    #     file.write(u'{}'.format(translation['translatedText']))
+    return (u'{}'.format(translation['translatedText']))
+    # print(u'Text: {}'.format(text))
+    # print(u'Translation: {}'.format(translation['translatedText']))
+    # [END translate_quickstart]
 
 # run the app.
 if __name__ == "__main__":
